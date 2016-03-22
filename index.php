258d248076a3f2d64787d066a830728adec7a17c
Snake Man <snakeman@phnet.fi>
2016-03-22 06:41:45 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Silent Hunter Submarine Simulation Series - PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Silent Hunter Submarine Simulation Series - PMC Tactical">
+<META name="keywords" content="Silent, Hunter, Submarine, Simulation, PMC, Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("../include/google-ads.php"); ?>
+<?php include("../include/paypal.php"); ?>
+<h1>TEMPLATE</h1>
+<p>Welcome to TEMPLATE.</p>
+</header>
+
+<section>
+</section>
+
+<footer>
+<p>Back to <a href="index.php">TEMPLATE root page</a></p>
+<?php include("../include/paypal.php"); ?>
+<br><br>
+<p><i>PMC TEMPLATE 2016 - <?print(date("Y"));?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
