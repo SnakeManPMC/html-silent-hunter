f6facb59226c04238ac4d47e06ec0920fdeffae7
Snake Man <snakeman@phnet.fi>
2022-07-02 15:28:15 +0000

    Added note about gamersgate

--- silent-hunter-4.php
+++ silent-hunter-4.php
@@ -4,7 +4,6 @@
 <title>Silent Hunter 4 - PMC Tactical</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="Silent Hunter 4 - PMC Tactical">
-<META name="keywords" content="Silent, Hunter, PMC, Tactical">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,21 +14,30 @@
 
 <header>
 <?php include("include/google-ads.php"); ?>
-<h1><b>PMC Tactical Silent Hunter 4</b></h1>
+	<h1>PMC Tactical Silent Hunter 4</h1>
 </header>
 
 <section>
-<h2>Silent Hunter 4</h2>
-<p>Latest released version is v1.5 which is only(?) available from U-Boot Missions expansion. Silent Hunter 4 (SH4) is American Silent Service submarine sim on Pacific Ocean against Japanese.
+	<h2>Silent Hunter 4</h2>
+
+<p>
+Latest released version is v1.5 which is only(?) available from U-Boot Missions expansion. Silent Hunter 4 (SH4) is American Silent Service submarine sim on Pacific Ocean against Japanese.
 </p>
 
-<p>SH4 also has a lot of mods created over the years.
+<p>
+SH4 also has a lot of mods created over the years.
 </p>
 
-<p>Since the modern Silent Hunter games 3 and 5 are both U-Boat stuff, SH4 is nice change where you get to play American skippers in Pacific. This game has odd movie grain type effect applied to it, you can decide if its good or bad.
+<p>
+Since the modern Silent Hunter games 3 and 5 are both U-Boat stuff, SH4 is nice change where you get to play American skippers in Pacific. This game has odd movie grain type effect applied to it, you can decide if its good or bad.
 </p>
 
-<p>Purchase SH4 from <a href="https://www.gamersgate.com/DD-SH4GE/silent-hunter-4-gold-edition">gamersgate, digital download and the version is Gold Edition</a> which includes U-Boat missions and v1.5 patch.
+<p>
+Purchase SH4 from <a href="https://www.gamersgate.com/product/silent-hunter-iv-wolves-of-the-pacific-gold-edition/" target="_blank">gamersgate, digital download and the version is Gold Edition</a> which includes U-Boat missions and v1.5 patch.
+</p>
+
+<p>
+Note: when the above link goes dead just go to gamersgate.com and search for "silent hunter 4 gold edition".
 </p>
 
 </section>
