8fba063b5a2e1b4d20285de948302560841e515f
Snake Man <snakeman@phnet.fi>
2024-03-03 11:56:33 +0000

    SH4 mod download with torrent magnet links

--- silent-hunter-4-downloads.php
+++ silent-hunter-4-downloads.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Silent Hunter 4 Downloads- PMC Tactical</title>
+<title>Silent Hunter 4 (SH4) Mod Downloads - PMC Tactical</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="Silent Hunter 4 Downloads- PMC Tactical">
-<META name="keywords" content="Silent, Hunter, Downloads, PMC, Tactical">
+<META name="description" content="Silent Hunter 4 (SH4) Mod Downloads - PMC Tactical">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,48 +13,51 @@
 <body>
 
 <header>
-<?php include("include/google-ads.php"); ?>
-<h1><b>PMC Tactical Silent Hunter 4 Downloads</b></h1>
+<h1>PMC Tactical Silent Hunter 4 (SH4) Mod Downloads</h1>
 </header>
 
 <section>
-<h2>Downloads</h2>
+	<h2>SH4 Mod Downloads</h2>
 
-<p>No hassle direct downloads without bullshit registrations, required donation, dead links, ads, scrolling endless page lenghts to get into download link or other annoying garbage. Just pure downloading enjoyment.
-</p>
-
-<p><a href="downloads/silent-hunter-4/RFB_2.0.7z">RFB_2.0.7z</a> Real Feet Boats v2.0 (RFB).
-</p>
-
-<p><a href="downloads/silent-hunter-4/TriggerMaru_Overhaul_2-5.7z">TriggerMaru_Overhaul_2-5.7z</a> Trigger Maru Overhaul v2.5 (TMO).
-</p>
-
-<p><a href="downloads/silent-hunter-4/Real_Environment_mod.7z">Real_Environment_mod.7z</a> Real Environment mod v1 By Kriller2 and W_clear
-</p>
-
-<p><a href="downloads/silent-hunter-4/Real_Graphics_Environmental_Mod.7z">Real_Graphics_Environmental_Mod.7z</a> no readme included, figures.
-</p>
-
-<p><a href="downloads/silent-hunter-4/SH4_Zoomable_Camera.7z">SH4_Zoomable_Camera.7z</a> readme didnt include authors name, figures.
-</p>
-
-<p><a href="downloads/silent-hunter-4/SH5_Water_for_SH4.7z">SH5_Water_for_SH4.7z</a> no readme included, figures.
-</p>
-
-<p><a href="downloads/silent-hunter-4/TMO_Water.7z">TMO_Water.7z</a> no readme included, figures.
-</p>
-
-<p><a href="downloads/silent-hunter-4/Updated_Newly_Revised_Environmental_Mod.7z">Updated_Newly_Revised_Environmental_Mod.7z</a> readme didnt include authors name, figures.
-</p>
-
-<p>If you have mods that you want to get added in this download page, please send them to us and we'll be happy to include them, or if you happen to know more details about these downloads please send that info as well.
-</p>
-
-<!--
 <p>
-<a href="downloads/silent-hunter-4/"></a>
+No hassle direct downloads without bullshit registrations, required donation, dead links, ads, scrolling endless page lenghts to get into download link or other annoying garbage. Just pure torrent downloading enjoyment. See also <a href="https://www.pmctactical.org/forum/viewtopic.php?f=28&t=22939" target="_blank">PMC Tactical forum Silenthunter.info website</a> topic.
 </p>
--->
+
+<p>
+RFB_2.0.7z Real Feet Boats v2.0 (RFB) - 391.7mb <a href="magnet:?xt=urn:btih:9933fa9c4a46cea899bc69fb92b5855a8f9338b2&xt=urn:btmh:1220df5b17150fcfcf15724d71547ff828815c4456a662c189c004c4c8837971d743&dn=RFB_2.0.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce" target="_blank">Torrent Magnet</a>.
+</p>
+
+<p>
+TriggerMaru_Overhaul_2-5.7z Trigger Maru Overhaul v2.5 (TMO) - 567mb <a href="magnet:?xt=urn:btih:5ceac6f49c95c74d862830f55ba2ef4e348e470c&xt=urn:btmh:122027ec06eade240cf7b26ecd28ba5d67d3d7461e27a268e2b7df5802241b8789ca&dn=TriggerMaru_Overhaul_2-5.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent Magnet</a>.
+</p>
+
+<p>
+Real_Environment_mod.7z Real Environment mod v1 By Kriller2 and W_clear - 48.5mb <a href="magnet:?xt=urn:btih:1e06affdae9c33d55a4f0c36d0f709d7e62134a5&xt=urn:btmh:1220db15d0078f2fe360ab22d1bbe0a24ad8214c0cc583e55f53fe622964bdb2cd05&dn=Real_Environment_mod.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent Magnet</a>.
+</p>
+
+<p>
+Real_Graphics_Environmental_Mod.7z no readme included, figures - 144mb <a href="magnet:?xt=urn:btih:8d39c53d2d3a88d51788c58ab6b5b1453a8aba94&xt=urn:btmh:1220dbf17a0075983477622f670fff283c5e16f00d33223f4046ae2a725bb7b0e446&dn=Real_Graphics_Environmental_Mod.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent Magnet</a>.
+</p>
+
+<p>
+SH4_Zoomable_Camera.7z readme didn't include authors name, figures - 836 bytes <a href="magnet:?xt=urn:btih:46787fe0be304898ca6a9e0598603be91635d2e1&xt=urn:btmh:122062195f072f125e7138e4a5b3b27e0c05193a9551bce776df2c082fd149aff7a7&dn=SH4_Zoomable_Camera.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent Magnet</a>.
+</p>
+
+<p>
+SH5_Water_for_SH4.7z no readme included, figures - 3mb <a href="magnet:?xt=urn:btih:882db6738c0c196f258339cabd187b141ac9829f&xt=urn:btmh:1220c1543ffc74e1f1e9fc709d71bb1ac4c266bcacfb0ee01199d9f876ee54cd5dd7&dn=SH5_Water_for_SH4.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent Magnet</a>.
+</p>
+
+<p>
+TMO_Water.7z no readme included, figures - 405.3kb <a href="magnet:?xt=urn:btih:004187a0050cb64d1d7553b5e2c5b245181abb34&xt=urn:btmh:12208d9ee898f44a1719d5ee1ecd4ecb847395f659cb91464de8efe5e391d3bbffdb&dn=TMO_Water.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent Magnet</a>.
+</p>
+
+<p>
+Updated_Newly_Revised_Environmental_Mod.7z readme didn't include authors name, figures - 8.5mb <a href="magnet:?xt=urn:btih:0c45011d062470fa58129c5f386595131ec27550&xt=urn:btmh:1220b8d6eff47a398dad45943f831eded96fbcc4f28c440ba42794f9557ebd705093&dn=Updated_Newly_Revised_Environmental_Mod.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent Magnet</a>.
+</p>
+
+<p>
+If you have mods that you want to get added in this download page, please send them to us and we'll be happy to include them, or if you happen to know more details about these downloads please send that info as well.
+</p>
 
 </section>
 
